351777820f67b394fbd24d04e4c2595c688c61ac
CoderCheese <blockpanther.9@gmail.com>
2021-03-23 08:40:14 +1300

    added computer score thing and a for loop

--- black_jack.py
+++ black_jack.py
@@ -4,21 +4,20 @@ listOfNumbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11]
 
 username = input("enter you username: ")
 
-score = 0
-computer_score = 0
+score, computer_score = 0, 0
 
 
-class player:
-    def __init__(self):
+class Player:
+    def __init__(self, score,computer_score):
         pass
 
     def main(self):
-        global score
         score = random.choice(listOfNumbers)
-        print(f"player 1 this is your deck {score}")
+        computer_score = random.choice(listOfNumbers)
+        print(f"player 1 this is your deck: {score}")
+        print(f"this is the computers score: {computer_score}")
 
     def turn(self):
-        global score
         turn = input("do you want to hit, stand or end(h,s,e)")
         if turn == "hit" or "h":
             score += random.choice(listOfNumbers)
@@ -29,7 +28,6 @@ class player:
             print("succesfully ended game")
 
     def check_if_game_end(self):
-        global score, computer_score
         if score == 21 and computer_score < 21:
             print(f"{username} wins")
         elif score == 21 and computer_score > 21:
@@ -40,6 +38,10 @@ class player:
             print("the computer won")
 
 
-p = player
+p = Player(score, computer_score)
 
-p.main("turn")
+p.main()
+
+for i in range(6):
+    p.turn()
+    p.check_if_game_end()

8e17cc878bd2ed0d605d201dd7431ffa8715da8c
CoderCheese <blockpanther.9@gmail.com>
2021-03-23 11:23:22 +1300

    fixed bug the local variable by using self.var instead of var

--- black_jack.py
+++ black_jack.py
@@ -4,44 +4,57 @@ listOfNumbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11]
 
 username = input("enter you username: ")
 
-score, computer_score = 0, 0
+score = 0
+
+computer_score = 0
 
 
 class Player:
-    def __init__(self, score,computer_score):
-        pass
+    def __init__(self, score,computer_score, listOfNumbers):
+        self.score = score
+        self.computer_score = computer_score
 
     def main(self):
-        score = random.choice(listOfNumbers)
-        computer_score = random.choice(listOfNumbers)
-        print(f"player 1 this is your deck: {score}")
-        print(f"this is the computers score: {computer_score}")
+        self.score += random.choice(listOfNumbers)
+        self.computer_score += random.choice(listOfNumbers)
+        print(f"player 1 this is your deck: {self.score}")
+        print(f"this is the computers score: {self.computer_score}")
 
     def turn(self):
         turn = input("do you want to hit, stand or end(h,s,e)")
         if turn == "hit" or "h":
-            score += random.choice(listOfNumbers)
-            print(score)
+            self.score += random.choice(listOfNumbers)
+            print(self.score)
         elif turn == "stand" or "s":
-            score += 0
+            self.score += 0
         elif turn == "end" or "e":
             print("succesfully ended game")
 
     def check_if_game_end(self):
-        if score == 21 and computer_score < 21:
+        if self.score == 21 and computer_score < 21:
             print(f"{username} wins")
-        elif score == 21 and computer_score > 21:
+        elif self.score == 21 and self.computer_score > 21:
             print(f"{username} wins")
-        elif score > 21:
+        elif self.score > 21:
             print("the computer won")
-        elif computer_score == 21 and score < 21:
+        elif self.computer_score == 21 and self.score < 21:
             print("the computer won")
+    def computer_turn():
+        if self.score == 20 and self.computer_score < 15:
+            self.computer_score += random.choice(listOfNumbers)
+        elif self.computer_score == 20:
+            self.computer_score += 0
+        elif self.computer_score <15 and score >15:
+            self.computer_score += random.choice(listOfNumbers)
 
 
-p = Player(score, computer_score)
+
+p = Player(score, computer_score, listOfNumbers)
 
 p.main()
 
 for i in range(6):
     p.turn()
     p.check_if_game_end()
+    '''p.computer_turn()
+    p.check_if_game_end()'''
